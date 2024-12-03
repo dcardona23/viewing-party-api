7bbb7dbf4750dd6fadb1f4766e9de36016bce5e3
Danielle Cardona <danielle.cardona23@gmail.com>
2024-12-02 21:11:53 -0700

    feat: add faraday connection to movies controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::MoviesController < ApplicationController
   def index
-    movies = Movie.all
+    conn = Faraday.new(url: "https://api.themoviedb.org") do |conn|
+      conn.authorization :Bearer, 014da2f6be8cc33649ff1955cae1b0e1
+    end
 
-    conn = Faraday.new(url: "https://api.themoviedb.org/3")
+    response = conn.get("/3/movie")
 
-    response = conn.get("/search/movie")
+    movies = JSON.parse(response.body)
   end
 
 end
