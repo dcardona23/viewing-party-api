d0b91e16e0856b2fbc5a4d4c26de621d587155fd
Danielle Cardona <danielle.cardona23@gmail.com>
2024-12-05 14:51:21 -0700

    remove invitees column from viewing parties table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_05_204049) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_05_215031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_05_204049) do
     t.string "movie_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "invitees", default: [], array: true
     t.integer "movie_id"
   end
 
